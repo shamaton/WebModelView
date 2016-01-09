79af759df4822a193f0c843aa0a2f353ea214eb5
shamaton <shamaton@hoge.com>
2016-01-09 19:11:12 +0900

    全体的にリファクタ

--- main.go
+++ main.go
@@ -40,159 +40,11 @@ var models = []string{
 	".mtl",
 }
 
-const TEMPLATE_DIR = "template/"
-
 var PRODUCTION = "0"
 var URL = "localhost"
 
 const PORT = "8080"
-
-/**************************************************************************************************/
-/*!
- *  テンプレート呼び出し
- */
-/**************************************************************************************************/
-func display(w http.ResponseWriter, tmpl string, data interface{}) {
-	html := tmpl + ".html"
-	var templates = template.Must(template.ParseFiles(TEMPLATE_DIR + html))
-	templates.ExecuteTemplate(w, tmpl+".html", data)
-}
-
-/**************************************************************************************************/
-/*!
- *  データアップロード
- */
-/**************************************************************************************************/
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	//GET displays the upload form.
-	case "GET":
-		var data = map[string]interface{}{}
-		data["models"] = strings.Join(models, " ")
-		display(w, "upload", data)
-
-	//POST takes the uploaded file(s) and saves it to disk.
-	case "POST":
-		isModelFind := false
-		//get the multipart reader for the request.
-		reader, err := r.MultipartReader()
-
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// アップロードするフォルダを設定
-		folder := strconv.FormatInt(time.Now().UnixNano(), 10)
-
-		// ディレクトリ取得
-		dir, err := os.Getwd()
-		if err != nil {
-			fmt.Fprintln(w, err)
-			return
-		}
-
-		// imagesがなければ作成
-		upd := path.Join(dir, "uploads")
-		uploadPath := path.Join(upd, folder)
-		if err := os.Mkdir(uploadPath, 0755); err != nil && !os.IsExist(err) {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		//copy each part to destination.
-		for {
-			part, err := reader.NextPart()
-			if err == io.EOF {
-				break
-			}
-
-			// ファイル名
-			fileName := part.FileName()
-
-			// 空で来たらスルー
-			if fileName == "" {
-				continue
-			}
-
-			// ファイル生成
-			dst, err := os.Create(uploadPath + "/" + fileName)
-
-			if err != nil {
-				dst.Close()
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			if _, err := io.Copy(dst, part); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			dst.Close()
-
-			// モデルデータチェック & JSON化
-			if isModelData(fileName) {
-				// 重複は許さない
-				if isModelFind {
-					http.Error(w, "複数のモデルデータは設定できません", http.StatusInternalServerError)
-					return
-				}
-
-				jsonData, errStr := jsonize(uploadPath, fileName)
-				if len(errStr) > 0 {
-					http.Error(w, errStr, http.StatusInternalServerError)
-					return
-				}
-
-				err := ioutil.WriteFile(uploadPath+"/data.json", jsonData, 0644)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-				isModelFind = true
-			}
-
-		}
-
-		// モデルデータがない場合もエラー
-		if !isModelFind {
-			http.Error(w, "モデルデータを含めてください", http.StatusInternalServerError)
-			return
-		}
-
-		// 生成したURLを返す
-		fmt.Fprintf(w, "http://"+URL+"/view?id="+folder)
-
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
-}
-
-/**************************************************************************************************/
-/*!
- *  サンプルモデルデータ表示
- */
-/**************************************************************************************************/
-func viewSampleHandler(w http.ResponseWriter, r *http.Request) {
-	display(w, "view_sample", nil)
-}
-
-/**************************************************************************************************/
-/*!
- *  モデルデータ表示
- */
-/**************************************************************************************************/
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-
-	// クエリパラメータをmapにする
-	m, _ := url.ParseQuery(r.URL.RawQuery)
-
-	// クエリパラメータを取得
-	var data = map[string]interface{}{}
-	data["Id"] = m["id"][0]
-
-	display(w, "view", data)
-}
+const TEMPLATE_DIR = "template/"
 
 /**************************************************************************************************/
 /*!
@@ -238,18 +90,13 @@ func main() {
 
 /**************************************************************************************************/
 /*!
- *  拡張子からモデルデータか判定
+ *  テンプレート呼び出し
  */
 /**************************************************************************************************/
-func isModelData(fileName string) bool {
-	ext := path.Ext(fileName)
-
-	for _, model := range models {
-		if model == ext || strings.ToUpper(model) == ext {
-			return true
-		}
-	}
-	return false
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	html := tmpl + ".html"
+	var templates = template.Must(template.ParseFiles(TEMPLATE_DIR + html))
+	templates.ExecuteTemplate(w, tmpl+".html", data)
 }
 
 /**************************************************************************************************/
@@ -271,6 +118,181 @@ func checkFolder() {
 		panic(err)
 		return
 	}
+}
+
+/****************************************** view **************************************************/
+/**************************************************************************************************/
+/*!
+ *  サンプルモデルデータ表示
+ */
+/**************************************************************************************************/
+func viewSampleHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "view_sample", nil)
+}
+
+/**************************************************************************************************/
+/*!
+ *  モデルデータ表示
+ */
+/**************************************************************************************************/
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+
+	// クエリパラメータをmapにする
+	m, _ := url.ParseQuery(r.URL.RawQuery)
+
+	// クエリパラメータを取得
+	var data = map[string]interface{}{}
+	data["Id"] = m["id"][0]
+
+	renderTemplate(w, "view", data)
+}
+
+/****************************************** upload **************************************************/
+/**************************************************************************************************/
+/*!
+ *  データアップロード
+ */
+/**************************************************************************************************/
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+
+	// GET : テンプレート表示
+	case "GET":
+		uploadGetMethod(w)
+
+	// POST : アップロード & 変換処理
+	case "POST":
+		uploadPostMethod(w, r)
+
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+/**************************************************************************************************/
+/*!
+ *  アップロード : GET
+ */
+/**************************************************************************************************/
+func uploadGetMethod(w http.ResponseWriter) {
+	var data = map[string]interface{}{}
+	data["models"] = strings.Join(models, " ")
+	renderTemplate(w, "upload", data)
+}
+
+/**************************************************************************************************/
+/*!
+ *  アップロード : POST
+ */
+/**************************************************************************************************/
+func uploadPostMethod(w http.ResponseWriter, r *http.Request) {
+	// チェッカー
+	isModelFind := false
+
+	//get the multipart reader for the request.
+	reader, err := r.MultipartReader()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// アップロードするフォルダを設定
+	folder := strconv.FormatInt(time.Now().UnixNano(), 10)
+
+	// ディレクトリ取得
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	// imagesがなければ作成
+	upd := path.Join(dir, "uploads")
+	uploadPath := path.Join(upd, folder)
+	if err := os.Mkdir(uploadPath, 0755); err != nil && !os.IsExist(err) {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//copy each part to destination.
+	for {
+		part, err := reader.NextPart()
+		if err == io.EOF {
+			break
+		}
+
+		// ファイル名
+		fileName := part.FileName()
+
+		// 空で来たらスルー
+		if fileName == "" {
+			continue
+		}
+
+		// ファイル生成
+		dst, err := os.Create(uploadPath + "/" + fileName)
+
+		if err != nil {
+			dst.Close()
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if _, err := io.Copy(dst, part); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		dst.Close()
+
+		// モデルデータチェック & JSON化
+		if isModelData(fileName) {
+			// 重複は許さない
+			if isModelFind {
+				http.Error(w, "複数のモデルデータは設定できません", http.StatusInternalServerError)
+				return
+			}
+
+			jsonData, errStr := jsonize(uploadPath, fileName)
+			if len(errStr) > 0 {
+				http.Error(w, errStr, http.StatusInternalServerError)
+				return
+			}
+
+			err := ioutil.WriteFile(uploadPath+"/data.json", jsonData, 0644)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			isModelFind = true
+		}
+
+	}
+
+	// モデルデータがない場合もエラー
+	if !isModelFind {
+		http.Error(w, "モデルデータを含めてください", http.StatusInternalServerError)
+		return
+	}
+
+	// 生成したURLを返す
+	fmt.Fprintf(w, "http://"+URL+"/view?id="+folder)
+}
+
+/**************************************************************************************************/
+/*!
+ *  拡張子からモデルデータか判定
+ */
+/**************************************************************************************************/
+func isModelData(fileName string) bool {
+	ext := path.Ext(fileName)
+
+	for _, model := range models {
+		if model == ext || strings.ToUpper(model) == ext {
+			return true
+		}
+	}
+	return false
 }
 
 /**************************************************************************************************/
